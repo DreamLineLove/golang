9c81f888fbec8b82eaa2aded0a415bdb1dce8416
DreamLineLove <zwenyanzaw@protonmail.com>
2023-06-09 14:37:50 +0630

    Explored structs

--- cmd/language/main.go
+++ cmd/language/main.go
@@ -2,7 +2,29 @@ package main
 
 import "fmt"
 
+type Water struct {
+	brand              string
+	price              float32
+	capacity_in_litres float32
+}
+
+type FastFoodChain struct {
+	BrandName      string
+	Revenue        float32
+	NumberOfChains int
+	secretFormula  string
+}
+
 func main() {
+	Alpine := Water{
+		brand:              "Alpine",
+		price:              300,
+		capacity_in_litres: 1,
+	}
+	fmt.Println(Alpine.brand)
+	fmt.Println(Alpine.price)
+	fmt.Println(Alpine.capacity_in_litres)
 
 	fmt.Println("Hello World")
+
 }
